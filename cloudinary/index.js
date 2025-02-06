e8d6cc250f5df72dccd7f7068c53af8cfccf635d
dylan-brosseau <dylanbrosseau2003@gmail.com>
2025-02-06 14:48:22 -0500

    Revert "Merge branch 'COIS-4000-Students' of https://github.com/uziarslan/hireddd-react-backend into COIS-4000-Students"
    This reverts commit d60c58bd380c4c7700e576fc6b4f46431f17f24c, reversing
    changes made to 6a80503458875be0f9a396d7e2840fb04ab173ea.

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -28,21 +28,7 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const deleteVideo = async (publicId) => {
-  try {
-    const fullPublicId = `Hireddd React/${publicId}`; // ✅ Add folder path
-    const result = await cloudinary.api.delete_resources([fullPublicId], { resource_type: "video" });
-
-    console.log("Cloudinary video deleted:", result);
-    return result;
-  } catch (error) {
-    console.error("Error deleting Cloudinary video:", error);
-    throw error;
-  }
-};
-
 module.exports = {
   cloudinary,
   storage,
-  deleteVideo,
 };
